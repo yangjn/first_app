a921bea35b5ffc11d88d4c6cdd71d9d0ddfbdf9f
unknown <yangjn@x200-yangjn-PC.(none)>
2012-05-17 23:59:18 +0800

    first_app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'c3f7193cdf4e208c33a48bfd6c5c43dccbf22a95e7d7a87bdeb1dbfd480ca4673e5b1bb04507a4b8a3b878ef6524db788cad131aa7698c828da8e8ad98b8e553'
